cc8ba1c2fedf69fe99f7a5cf718a10868d441729
Carolina Tovar <carolina.ta13@gmail.com>
2023-04-19 16:57:03 -0600

    Se edito la información de la página de productos individuales

--- assets/js/plantillaProducto.js
+++ assets/js/plantillaProducto.js
@@ -1,17 +1,15 @@
-let jsonRapido=[
-    {
-        "id": 1,
-        "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-        "price": 109.95,
-        "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-        "category": "men's clothing",
-        "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-        "rating": {
-        "rate": 3.9,
-        "count": 120
-        }
-        }
-    ];
+let jsonRapido=
+    [
+        {id: 1,
+        corte: "Ribeye",
+        marca:"Kagura",
+        calidad:"A5",
+        origen:"Japon",
+        gramos:"XXXX",
+        precio:5750,
+        image:"https://www.edualimentaria.com/images/carnes/carnes-derivados-cecinas.jpg",
+        descripcion:"El preferido de muchos por su gran sabor y suavidad. El Rib-Eye es un corte que abarca la parte superior de la costilla y la carne incluida en ambos lados de la misma."
+    }];
 
     let producto = document.getElementsByClassName("producto");
 
@@ -26,29 +24,27 @@ let jsonRapido=[
                 <img src="${e.image}" alt="" class="imagen">
                 </div>
                 <div class="productoTexto">
-                    <h1>${e.title}</h1>
-                    <h2>Marca</h2>
-                    <h3>Calidad</h3>
-                    <h3>Origen</h3>
+                    <h1>${e.corte}</h1>
+                    <h2>${e.marca}</h2>
+                    <h3>${e.calidad}</h3>
+                    <h3>${e.origen}</h3>
                     <h4 style="color:rgb(20, 179, 20)">Existencia</h4>
                     <div class="dropdown">
                         <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                           Seleccionar
                         </button>
                         <ul class="dropdown-menu">
-                          <li><a class="dropdown-item" href="#">Action</a></li>
-                          <li><a class="dropdown-item" href="#">Another action</a></li>
-                          <li><a class="dropdown-item" href="#">Something else here</a></li>
+                          <li><a class="dropdown-item" href="#">Aquí</a></li>
+                          <li><a class="dropdown-item" href="#">Va</a></li>
+                          <li><a class="dropdown-item" href="#">Inventario</a></li>
                         </ul>
                       </div>
                       <div class="precio">
-                      <h2 style="color:rgba(184, 28, 0, 1)">Precio por kilo</h2>
+                      <h2 style="color:rgba(184, 28, 0, 1)">$${e.precio}</h2>
                       <button type="button" class="btnCompra">Agregar al carrito</button>
                     
                       </div>
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                        Id, autem ipsa. Architecto beatae, 
-                        sit id aspernatur mollitia sapiente delectus. Cum.</p>
+                      <p>${e.descripcion}</p>
     
                 </div>`;
             
