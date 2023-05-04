268998d25ceabe7362b8f21168e96f09cb75193e
Moicr96 <moicr96@gmail.com>
2023-05-04 17:41:57 -0600

    Se sube la base de datos de acuerdo a la tarea 11

---
+++ BaseDeDatos/3. Scripts para insertar registros en las tablas).sql
@@ -0,0 +1,203 @@
+USE vianda_market_db;
+-- ########### RIBEYE ###########
+-- Ribeye precio 5750
+INSERT INTO cortes (corte, precio, descripcion_corte, disponibilidad, cantidad_disponible, idcalidades)
+VALUE ('Ribeye','5750',
+'El preferido de muchos, esto gracias a su balance de sabor, suavidad y cantidad de grasa, es un corte sin hueso que proviene de la parte superior de las costillas.',
+'1','5','1');
+
+-- Ribeye precio 3850
+INSERT INTO cortes (corte, precio, descripcion_corte, disponibilidad, cantidad_disponible, idcalidades)
+VALUE ('Ribeye','3850',
+'El preferido de muchos, esto gracias a su balance de sabor, suavidad y cantidad de grasa, es un corte sin hueso que proviene de la parte superior de las costillas.',
+'1','3','2');
+
+-- Ribeye  precio 3170
+INSERT INTO cortes (corte, precio, descripcion_corte, disponibilidad, cantidad_disponible, idcalidades)
+VALUE ('Ribeye','3170',
+'El preferido de muchos, esto gracias a su balance de sabor, suavidad y cantidad de grasa, es un corte sin hueso que proviene de la parte superior de las costillas.',
+'1','3','3');
+
+-- Ribeye precio 1250
+INSERT INTO cortes (corte, precio, descripcion_corte, disponibilidad, cantidad_disponible, idcalidades)
+VALUE ('Ribeye','1250',
+'El preferido de muchos, esto gracias a su balance de sabor, suavidad y cantidad de grasa, es un corte sin hueso que proviene de la parte superior de las costillas.',
+'1','3','4');
+
+
+-- Ribeye precio 698
+INSERT INTO cortes (corte, precio, descripcion_corte, disponibilidad, cantidad_disponible, idcalidades)
+VALUE ('Ribeye','698',
+'El preferido de muchos, esto gracias a su balance de sabor, suavidad y cantidad de grasa, es un corte sin hueso que proviene de la parte superior de las costillas.',
+'1','3','7');
+
+-- ########### NEW YORK ###########
+-- Newyork precio 5750
+INSERT INTO cortes (corte, precio, descripcion_corte, disponibilidad, cantidad_disponible, idcalidades)
+VALUE ('New York','5750',
+'Es un corte muy bien valorado, viene del lomo de la res. Es popular por su gran suavidad y poca grasa.',
+'1','3','1');
+-- Newyork precio 3790
+INSERT INTO cortes (corte, precio, descripcion_corte, disponibilidad, cantidad_disponible, idcalidades)
+VALUE ('New York','3790',
+'Es un corte muy bien valorado, viene del lomo de la res. Es popular por su gran suavidad y poca grasa.',
+'1','3','2');
+-- Newyork precio 3150
+INSERT INTO cortes (corte, precio, descripcion_corte, disponibilidad, cantidad_disponible, idcalidades)
+VALUE ('New York','3150',
+'Es un corte muy bien valorado, viene del lomo de la res. Es popular por su gran suavidad y poca grasa.',
+'1','3','3');
+-- Newyork precio 1160
+INSERT INTO cortes (corte, precio, descripcion_corte, disponibilidad, cantidad_disponible, idcalidades)
+VALUE ('New York','1160',
+'Es un corte muy bien valorado, viene del lomo de la res. Es popular por su gran suavidad y poca grasa.',
+'1','3','4');
+-- Newyork precio 456
+INSERT INTO cortes (corte, precio, descripcion_corte, disponibilidad, cantidad_disponible, idcalidades)
+VALUE ('New York','456',
+'Es un corte muy bien valorado, viene del lomo de la res. Es popular por su gran suavidad y poca grasa.',
+'1','3','7');
+-- ########### PORTERHOUSE ###########
+-- Porterhouse 3500
+INSERT INTO cortes (corte, precio, descripcion_corte, disponibilidad, cantidad_disponible, idcalidades)
+VALUE ('Porterhouse','3500',
+'Muy conocido por ser agradable a la vista, pero tambien por su peculiaridad, esta conformado por dos cortes muy populares, el New York y Filete, lo que crea un balance sin igual.
+Mas conocido como "T-Bone" pero la diferencia es el tamaño de la porcion de Filete, se llama Porterhouse si el filete mide al menos 1.25 pulgadas de grosor.',
+'1','3','3');
+-- Porterhouse 790
+INSERT INTO cortes (corte, precio, descripcion_corte, disponibilidad, cantidad_disponible, idcalidades)
+VALUE ('Porterhouse','790',
+'Muy conocido por ser agradable a la vista, pero tambien por su peculiaridad, esta conformado por dos cortes muy populares, el New York y Filete, lo que crea un balance sin igual.
+Mas conocido como "T-Bone" pero la diferencia es el tamaño de la porcion de Filete, se llama Porterhouse si el filete mide al menos 1.25 pulgadas de grosor.',
+'1','3','4');
+-- ########### T-BONE ###########
+-- T-Bone 3450
+INSERT INTO cortes (corte, precio, descripcion_corte, disponibilidad, cantidad_disponible, idcalidades)
+VALUE ('T-Bone','3450',
+'El corte perfecto para tener un balance entre dos cortes, por un lado tenemos el Filete, 
+conocido por ser el corte mas blando y por el otro lados el New York, uno de los favoritos de la parrilla por su suavidad y sabor.',
+'1','3','3');
+-- T-Bone 470
+INSERT INTO cortes (corte, precio, descripcion_corte, disponibilidad, cantidad_disponible, idcalidades)
+VALUE ('T-Bone','470',
+'El corte perfecto para tener un balance entre dos cortes, por un lado tenemos el Filete, 
+conocido por ser el corte mas blando y por el otro lados el New York, uno de los favoritos de la parrilla por su suavidad y sabor.',
+'1','3','4');
+-- ########### TOMAHAWK ###########
+-- Tomahawk 3250
+INSERT INTO cortes (corte, precio, descripcion_corte, disponibilidad, cantidad_disponible, idcalidades)
+VALUE ('Tomahawk','3250',
+'El corte mas popular en la actualidad por su increible apariencia y tamaño, el nombre lo toma de una "Hacha" por su aspecto, este es un ribeye con hueso, 
+lo que ayuda a compartir ese increible sabor del hueso y evitar perdida de jugos, una excelente eleccion para cualquier situacion que lo necesites.',
+'1','3','3');
+-- ########### PICAÑA ###########
+-- PICAÑA 1700
+INSERT INTO cortes (corte, precio, descripcion_corte, disponibilidad, cantidad_disponible, idcalidades)
+VALUE ('Picaña','1700',
+'Popularizado por su reconocimiento en espadas Brasileñas, es un corte de gran sabor y suavidad, excelente para asar en laminas pero tambien para el ahumado.',
+'1','3','3');
+-- PICAÑA 500
+INSERT INTO cortes (corte, precio, descripcion_corte, disponibilidad, cantidad_disponible, idcalidades)
+VALUE ('Picaña','500',
+'Popularizado por su reconocimiento en espadas Brasileñas, es un corte de gran sabor y suavidad, excelente para asar en laminas pero tambien para el ahumado.',
+'1','3','6');
+-- PICAÑA 367
+INSERT INTO cortes (corte, precio, descripcion_corte, disponibilidad, cantidad_disponible, idcalidades)
+VALUE ('Picaña','367',
+'Popularizado por su reconocimiento en espadas Brasileñas, es un corte de gran sabor y suavidad, excelente para asar en laminas pero tambien para el ahumado.',
+'1','3','7');
+-- ########### SHORT RIBS ###########
+-- SHORT RIBS 720
+INSERT INTO cortes (corte, precio, descripcion_corte, disponibilidad, cantidad_disponible, idcalidades)
+VALUE ('Short Ribs','720',
+'La costilla cargada es exquisita, su balance de carne y hueso le da un tremendo sabor y suavidad, 
+tambien por tener un abundante marmoleo desde las calidades mas bajas. Excelente opcion para la botana o ahumado.',
+'1','3','6');
+-- ########### VACÍO ARGENTINO ###########
+-- VACÍO ARGENTINO 750
+INSERT INTO cortes (corte, precio, descripcion_corte, disponibilidad, cantidad_disponible, idcalidades)
+VALUE ('Vacío Argentino','750',
+'Corte que apenas esta ganando popularidad, su textura es similar a la arrachera pero con mucho mas sabor y suavidad.',
+'1','3','6');
+
+SELECT * FROM vianda_market_db.cortes;
+
+-- ########### TABLA USUARIOS ###########
+
+INSERT INTO Usuarios (domicilio, nombres, apellidos, correo, contrasena, edad)
+VALUES ( "Eje Central, Colonia Zapata", "Francisco", "Salinas", "FranSal@gmail.com", "#HolaMundo1", 27);
+INSERT INTO Usuarios (domicilio, nombres, apellidos, correo, contrasena, edad)
+VALUES ( "Avenida Libertador, Colonia Paso Limonero ", "Gerardo", "Mendoza", "MenGer@gmail.com", "%Rinoceronte23", 25);
+INSERT INTO Usuarios (domicilio, nombres, apellidos, correo, contrasena, edad)
+VALUES ( "Avenida Unidos, Colonia Renacimiento", "Moises", "Rodriguez", "Moi@gmail.com", "&Nopales95", 23);
+INSERT INTO Usuarios (domicilio, nombres, apellidos, correo, contrasena, edad)
+VALUES ( "Avenida Renacimiento, Colonia Las Cruces", "Andrea", "Hernandez", "Andrea@gmail.com", "*Automobil7", 29);
+INSERT INTO Usuarios (domicilio, nombres, apellidos, correo, contrasena, edad)
+VALUES ( "Avenida Lopez Mateos, Colonia Coasta Azul", "Carolina", "Lopez", "Caro@gmail.com", "@Europa85", 31);
+SELECT * FROM vianda_market_db.usuarios;
+
+-- ########### TABLA COMPLEMENTOS ###########
+
+
+INSERT INTO complementos (idcomplementos, nombre, disponibilidad, cantidad_disponible, descripcion_complemento, precio)
+VALUE (1,'Chistorra',0,1,'Este producto similar a la salchicha parrillera sudamericana pero con diferencias en el sabor. Se puede decir que es un producto con sabores más españoles dados por los condimentos utilizados.',248);
+INSERT INTO complementos (idcomplementos,nombre, disponibilidad, cantidad_disponible, descripcion_complemento, precio)
+VALUE (2,'Chorizo Argentino',1,1,'También conocido como chorizo criollo, utilizado en algunas ocasiones para chori panes, ideal para asarlo o a la parrilla.',204);
+INSERT INTO complementos (idcomplementos,nombre, disponibilidad, cantidad_disponible, descripcion_complemento, precio)
+VALUE (3,'Termómetro',1,1,'El nuevo termómetro de alimentos de lectura rápida proporciona lecturas precisas de la temperatura de los alimentos en segundos. El termómetro cabe cómodamente en su bolsillo.',420);
+SELECT * FROM vianda_market_db.complementos;
+
+-- ########### TABLA GRAMOS ###########
+
+
+INSERT INTO gramos (gramos, cortes_idcortes)
+VALUES (350,1), (350,6), (370,1), (370,6), (400,1), (400,6), 
+        (420,2), (440,2), (470,2), (490,2), 
+        (360,7),(360,3),(360,8),
+       (380,7),(380,3),(380,8),
+       (430,7),(430,3),(430,8),
+       (770,11),(800,11),
+        (760,13),(780,13),
+        (1200,15),(2000,16),
+        (520,4),(1000,4),
+        (330,9),(350,9),(370,9),
+         (900,12),(1000,12),
+         (720,14),(750,14),
+          (1500,17),(2000,17),(2500,17),
+           (400,20),(700,20),
+           (380,5),(400,5),(420,5),(450,5),
+           (350,10),(370,10),(400,10),
+           (1700,18),(2000,18);
+
+ INSERT INTO gramos (gramos, complementos_idcomplementos)
+VALUES
+           (250,1),(500,1),(750,1), (1000,1),
+            (250,2),(500,2),(750,2), (1000,2);
+
+SELECT * FROM vianda_market_db.gramos;
+
+-- ########### TABLA CALIDADES ###########
+
+INSERT INTO calidades (marca, pais, descripcion_marca, calidad)
+VALUE ('Japanese Beef', 'Japon', 
+'La carne Japonesa es la mejor del mundo, su certificacion refleja autenticidad y calidad, nuestros productos cuentan con el grado A5 BMS 8-11, lo mejor en la escala Japonesa.', 
+'A5 (BMS 8-11)');
+INSERT INTO calidades (marca, pais, descripcion_marca, calidad)
+VALUE ('Stone Axe', 'Australia', 'Stone Axe es un Wagyu Fullblood Australiano con BMS 9+, lo mas alto en la escala de marmoleo australiana.',
+ 'BMS 9+');
+INSERT INTO calidades (marca, pais, descripcion_marca, calidad)
+VALUE ('Margaret River', 'Australia', 'Margaret River es un Wagyu Pure Bred con BMS 8-9, su genetica Wagyu esta en el 94%, un marmoleo y sabor excepcional.', 
+'BMS 8-9');
+INSERT INTO calidades (marca, pais, descripcion_marca, calidad)
+VALUE ('Excel', 'USA / Canada', 'Excel es uno de los mayores comercializadores de carne en norte america, sin duda, una garantia obtener sus productos.', 
+'Prime (BMS 4-5)');
+INSERT INTO calidades (marca, pais, descripcion_marca, calidad)
+VALUE ('CAB', 'USA / Canada', 'CAB con sus 10 normas adicionales a la USDA genera una gran confianza a garantia a sus clientes, una excelente opcion para saborear lo mejor de USA y Canada.', 
+'Prime (BMS 4-5)');
+INSERT INTO calidades ( marca, pais, descripcion_marca, calidad)
+VALUE ('Sterling Silver', 'USA / Canada', 'Famosa por sus productos High Choice, lo mejor dentro de su categoria. Su proceso de añejamiento de 21 potencializa el sabor y suavidad de la carne.', 
+'High Choice (BMS 3)');
+INSERT INTO calidades (marca, pais, descripcion_marca, calidad)
+VALUE (' ', 'México', 'El producto que manejamos en esta categoria proviene de Sonora, con un excelente sabor, calidad y textura. Su marmoleo es similar a los productos Choice y Prime.',
+ 'Nacional');
+SELECT * FROM vianda_market_db.calidades;
