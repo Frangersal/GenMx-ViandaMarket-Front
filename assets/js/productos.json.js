fdabc27c43f101bec11ec2a2a0b829667f578e50
Moicr96 <moicr96@gmail.com>
2023-06-14 10:46:30 -0600

    se cambian las imagenes a formato webp así como ajustar las referencias a las img

--- assets/js/productos.json.js
+++ assets/js/productos.json.js
@@ -7,7 +7,7 @@ export let productos = [
     "calidad": "A5 (BMS 8-11)",
     "origen": "Japon",
     "precio": "5.750,00",
-    "imagen": "https://res.cloudinary.com/dw1mig7ts/image/upload/v1682899814/ViandaMarket/Ribeye-wagyujapones3_a3zdff.jpg",
+    "imagen": "https://res.cloudinary.com/dw1mig7ts/image/upload/v1682899814/ViandaMarket/Ribeye-wagyujapones3_a3zdff.webp",
     "gramos": "350 / 370 / 400",
     "descripcionCorte": "El preferido de muchos, esto gracias a su balance de sabor, suavidad y cantidad de grasa, es un corte sin hueso que proviene de la parte superior de las costillas.",
     "descripcionMarca": "La carne Japonesa es la mejor del mundo, su certificacion refleja autenticidad y calidad, nuestros productos cuentan con el grado A5 BMS 8-11, lo mejor en la escala Japonesa."
@@ -19,7 +19,7 @@ export let productos = [
     "calidad": "A5 (BMS 8-11)",
     "origen": "Japon",
     "precio": "5.750,00",
-    "imagen": "https://res.cloudinary.com/dw1mig7ts/image/upload/v1682899806/ViandaMarket/NewYork_-wagyustoneaxe3_wgtwty.jpg",
+    "imagen": "https://res.cloudinary.com/dw1mig7ts/image/upload/v1682899806/ViandaMarket/NewYork_-wagyustoneaxe3_wgtwty.webp",
     "gramos": "350 / 370 / 400",
     "descripcionCorte": "Es un corte muy bien valorado, viene del lomo de la res. Es popular por su gran suavidad y poca grasa.",
     "descripcionMarca": "La carne Japonesa es la mejor del mundo, su certificacion refleja autenticidad y calidad, nuestros productos cuentan con el grado A5 BMS 8-11, lo mejor en la escala Japonesa."
@@ -31,7 +31,7 @@ export let productos = [
     "calidad": "BMS 9+",
     "origen": "Australia",
     "precio": "3.850,00",
-    "imagen": "https://res.cloudinary.com/dw1mig7ts/image/upload/v1682899816/ViandaMarket/Ribeye-wagyustoneaxe3_oki70a.jpg",
+    "imagen": "https://res.cloudinary.com/dw1mig7ts/image/upload/v1682899816/ViandaMarket/Ribeye-wagyustoneaxe3_oki70a.webp",
     "gramos": "420 / 440 / 470 / 490",
     "descripcionCorte": "El preferido de muchos, esto gracias a su balance de sabor, suavidad y cantidad de grasa, es un corte sin hueso que proviene de la parte superior de las costillas.",
     "descripcionMarca": "Stone Axe es un Wagyu Fullblood Australiano con BMS 9+, lo mas alto en la escala de marmoleo australiana."
@@ -43,7 +43,7 @@ export let productos = [
     "calidad": "BMS 9+",
     "origen": "Australia",
     "precio": "3.790,00",
-    "imagen": "https://res.cloudinary.com/dw1mig7ts/image/upload/v1682899806/ViandaMarket/NewYork_-wagyustoneaxe2_gwu63x.jpg",
+    "imagen": "https://res.cloudinary.com/dw1mig7ts/image/upload/v1682899806/ViandaMarket/NewYork_-wagyustoneaxe2_gwu63x.webp",
     "gramos": "360 / 380 / 430",
     "descripcionCorte": "Es un corte muy bien valorado, viene del lomo de la res. Es popular por su gran suavidad y poca grasa.",
     "descripcionMarca": "Stone Axe es un Wagyu Fullblood Australiano con BMS 9+, lo mas alto en la escala de marmoleo australiana."
@@ -55,7 +55,7 @@ export let productos = [
     "calidad": "BMS 8-9",
     "origen": "Australia",
     "precio": "3.170,00",
-    "imagen": "https://res.cloudinary.com/dw1mig7ts/image/upload/v1682899809/ViandaMarket/Ribeye_-Margaret3_to5nqd.jpg",
+    "imagen": "https://res.cloudinary.com/dw1mig7ts/image/upload/v1682899809/ViandaMarket/Ribeye_-Margaret3_to5nqd.webp",
     "gramos": "360 / 380 / 430",
     "descripcionCorte": "El preferido de muchos, esto gracias a su balance de sabor, suavidad y cantidad de grasa, es un corte sin hueso que proviene de la parte superior de las costillas.",
     "descripcionMarca": "Margaret River es un Wagyu Pure Bred con BMS 8-9, su genetica Wagyu esta en el 94%, un marmoleo y sabor excepcional."
@@ -67,7 +67,7 @@ export let productos = [
     "calidad": "BMS 8-9",
     "origen": "Australia",
     "precio": "3.150,00",
-    "imagen": "https://res.cloudinary.com/dw1mig7ts/image/upload/v1682899806/ViandaMarket/NewYork_-wagyustoneaxe3_wgtwty.jpg",
+    "imagen": "https://res.cloudinary.com/dw1mig7ts/image/upload/v1682899806/ViandaMarket/NewYork_-wagyustoneaxe3_wgtwty.webp",
     "gramos": "360 / 380 / 430",
     "descripcionCorte": "Es un corte muy bien valorado, viene del lomo de la res. Es popular por su gran suavidad y poca grasa.",
     "descripcionMarca": "Margaret River es un Wagyu Pure Bred con BMS 8-9, su genetica Wagyu esta en el 94%, un marmoleo y sabor excepcional."
@@ -79,7 +79,7 @@ export let productos = [
     "calidad": "BMS 8-9",
     "origen": "Australia",
     "precio": "3.500,00",
-    "imagen": "https://res.cloudinary.com/dw1mig7ts/image/upload/v1682899809/ViandaMarket/Porterhouse-Margaret5_g3fmm0.jpg",
+    "imagen": "https://res.cloudinary.com/dw1mig7ts/image/upload/v1682899809/ViandaMarket/Porterhouse-Margaret5_g3fmm0.webp",
     "gramos": "770 / 800",
     "descripcionCorte": "Muy conocido por ser agradable a la vista, pero tambien por su peculiaridad, esta conformado por dos cortes muy populares, el New York y Filete, lo que crea un balance sin igual.\n\nMas conocido como \"T-Bone\" pero la diferencia es el tamaño de la porcion de Filete, se llama Porterhouse si el filete mide al menos 1.25 pulgadas de grosor.",
     "descripcionMarca": "Margaret River es un Wagyu Pure Bred con BMS 8-9, su genetica Wagyu esta en el 94%, un marmoleo y sabor excepcional."
@@ -91,7 +91,7 @@ export let productos = [
     "calidad": "BMS 8-9",
     "origen": "Australia",
     "precio": "3.450,00",
-    "imagen": "https://res.cloudinary.com/dw1mig7ts/image/upload/v1682899821/ViandaMarket/Tbone-Margaret2_xvhnqc.jpg",
+    "imagen": "https://res.cloudinary.com/dw1mig7ts/image/upload/v1682899821/ViandaMarket/Tbone-Margaret2_xvhnqc.webp",
     "gramos": "760 / 780",
     "descripcionCorte": "El corte perfecto para tener un balance entre dos cortes, por un lado tenemos el Filete, conocido por ser el corte mas blando y por el otro lados el New York, uno de los favoritos de la parrilla por su suavidad y sabor.",
     "descripcionMarca": "Margaret River es un Wagyu Pure Bred con BMS 8-9, su genetica Wagyu esta en el 94%, un marmoleo y sabor excepcional."
@@ -103,7 +103,7 @@ export let productos = [
     "calidad": "BMS 8-9",
     "origen": "Australia",
     "precio": "3.250,00",
-    "imagen": "https://res.cloudinary.com/dw1mig7ts/image/upload/v1682899806/ViandaMarket/NewYork_-wagyustoneaxe3_wgtwty.jpg",
+    "imagen": "https://res.cloudinary.com/dw1mig7ts/image/upload/v1682899806/ViandaMarket/NewYork_-wagyustoneaxe3_wgtwty.webp",
     "gramos": "1.2kg",
     "descripcionCorte": "El corte mas popular en la actualidad por su increible apariencia y tamaño, el nombre lo toma de una \"Hacha\" por su aspecto, este es un ribeye con hueso, lo que ayuda a compartir ese increible sabor del hueso y evitar perdida de jugos, una excelente eleccion para cualquier situacion que lo necesitres.",
     "descripcionMarca": "Margaret River es un Wagyu Pure Bred con BMS 8-9, su genetica Wagyu esta en el 94%, un marmoleo y sabor excepcional."
@@ -115,7 +115,7 @@ export let productos = [
     "calidad": "BMS 8-9",
     "origen": "Australia",
     "precio": "1.700,00",
-    "imagen": "https://res.cloudinary.com/dw1mig7ts/image/upload/v1682899806/ViandaMarket/NewYork_-wagyustoneaxe3_wgtwty.jpg",
+    "imagen": "https://res.cloudinary.com/dw1mig7ts/image/upload/v1682899806/ViandaMarket/NewYork_-wagyustoneaxe3_wgtwty.webp",
     "gramos": "2kg",
     "descripcionCorte": "Popularizado por su reconocimiento en espadas Brasileñas, es un corte de gran sabor y suavidad, excelente para asar en laminas pero tambien para el ahumado.",
     "descripcionMarca": "Margaret River es un Wagyu Pure Bred con BMS 8-9, su genetica Wagyu esta en el 94%, un marmoleo y sabor excepcional."
@@ -127,7 +127,7 @@ export let productos = [
     "calidad": "Prime (BMS 4-5)",
     "origen": "USA / Canada",
     "precio": "1.250,00",
-    "imagen": "https://res.cloudinary.com/dw1mig7ts/image/upload/v1682899817/ViandaMarket/Ribeye-wagyustoneaxe4_qvdaoi.jpg",
+    "imagen": "https://res.cloudinary.com/dw1mig7ts/image/upload/v1682899817/ViandaMarket/Ribeye-wagyustoneaxe4_qvdaoi.webp",
     "gramos": "520 / 1kg",
     "descripcionCorte": "El preferido de muchos, esto gracias a su balance de sabor, suavidad y cantidad de grasa, es un corte sin hueso que proviene de la parte superior de las costillas.",
     "descripcionMarca": "CAB con sus 10 normas adicionales a la USDA genera una gran confianza a garantia a sus clientes, una excelente opcion para saborear lo mejor de USA y Canada"
@@ -139,7 +139,7 @@ export let productos = [
     "calidad": "Prime (BMS 4-5)",
     "origen": "USA / Canada",
     "precio": "1.160,00",
-    "imagen": "https://res.cloudinary.com/dw1mig7ts/image/upload/v1682899806/ViandaMarket/NewYork_-wagyustoneaxe3_wgtwty.jpg",
+    "imagen": "https://res.cloudinary.com/dw1mig7ts/image/upload/v1682899806/ViandaMarket/NewYork_-wagyustoneaxe3_wgtwty.webp",
     "gramos": "330 / 350 / 370",
     "descripcionCorte": "Es un corte muy bien valorado, viene del lomo de la res. Es popular por su gran suavidad y poca grasa.",
     "descripcionMarca": "Excel es uno de los mayores comercializadores de carne en norte america, sin duda, una garantia obtener sus productos."
@@ -151,7 +151,7 @@ export let productos = [
     "calidad": "Prime (BMS 4-5)",
     "origen": "USA / Canada",
     "precio": "790,00",
-    "imagen": "https://res.cloudinary.com/dw1mig7ts/image/upload/v1682899806/ViandaMarket/NewYork_-wagyustoneaxe3_wgtwty.jpg",
+    "imagen": "https://res.cloudinary.com/dw1mig7ts/image/upload/v1682899806/ViandaMarket/NewYork_-wagyustoneaxe3_wgtwty.webp",
     "gramos": "900 / 1kg",
     "descripcionCorte": "Muy conocido por ser agradable a la vista, pero tambien por su peculiaridad, esta conformado por dos cortes muy populares, el New York y Filete, lo que crea un balance sin igual.\n\nMas conocido como \"T-Bone\" pero la diferencia es el tamaño de la porcion de Filete, se llama Porterhouse si el filete mide al menos 1.25 pulgadas de grosor.",
     "descripcionMarca": "CAB con sus 10 normas adicionales a la USDA genera una gran confianza a garantia a sus clientes, una excelente opcion para saborear lo mejor de USA y Canada"
@@ -163,7 +163,7 @@ export let productos = [
     "calidad": "Prime (BMS 4-5)",
     "origen": "USA / Canada",
     "precio": "470,00",
-    "imagen": "https://res.cloudinary.com/dw1mig7ts/image/upload/v1682899806/ViandaMarket/NewYork_-wagyustoneaxe3_wgtwty.jpg",
+    "imagen": "https://res.cloudinary.com/dw1mig7ts/image/upload/v1682899806/ViandaMarket/NewYork_-wagyustoneaxe3_wgtwty.webp",
     "gramos": "720 / 750",
     "descripcionCorte": "El corte perfecto para tener un balance entre dos cortes, por un lado tenemos el Filete, conocido por ser el corte mas blando y por el otro lados el New York, uno de los favoritos de la parrilla por su suavidad y sabor.",
     "descripcionMarca": "Excel es uno de los mayores comercializadores de carne en norte america, sin duda, una garantia obtener sus productos."
@@ -175,7 +175,7 @@ export let productos = [
     "calidad": "High Choice (BMS 3)",
     "origen": "USA / Canada",
     "precio": "500,00",
-    "imagen": "https://res.cloudinary.com/dw1mig7ts/image/upload/v1682899806/ViandaMarket/NewYork_-wagyustoneaxe3_wgtwty.jpg",
+    "imagen": "https://res.cloudinary.com/dw1mig7ts/image/upload/v1682899806/ViandaMarket/NewYork_-wagyustoneaxe3_wgtwty.webp",
     "gramos": "1.5kg / 2kg / 2.5kg",
     "descripcionCorte": "Popularizado por su reconocimiento en espadas Brasileñas, es un corte de gran sabor y suavidad, excelente para asar en laminas pero tambien para el ahumado.",
     "descripcionMarca": "Famosa por sus productos High Choice, lo mejor dentro de su categoria. Su proceso de añejamiento de 21 potencializa el sabor y suavidad de la carne."
@@ -187,7 +187,7 @@ export let productos = [
     "calidad": "High Choice (BMS 3)",
     "origen": "USA / Canada",
     "precio": "720,00",
-    "imagen": "https://res.cloudinary.com/dw1mig7ts/image/upload/v1682899819/ViandaMarket/ShortRibs-Prime2_phxmxx.jpg",
+    "imagen": "https://res.cloudinary.com/dw1mig7ts/image/upload/v1682899819/ViandaMarket/ShortRibs-Prime2_phxmxx.webp",
     "gramos": "",
     "descripcionCorte": "La costilla cargada es exquisita, su balance de carne y hueso le da un tremendo sabor y suavidad, tambien por tener un abundante marmoleo desde las calidades mas bajas. Excelente opcion para la botana o ahumado.",
     "descripcionMarca": "Famosa por sus productos High Choice, lo mejor dentro de su categoria. Su proceso de añejamiento de 21 potencializa el sabor y suavidad de la carne."
@@ -199,7 +199,7 @@ export let productos = [
     "calidad": "High Choice (BMS 3)",
     "origen": "USA / Canada",
     "precio": "750,00",
-    "imagen": "https://res.cloudinary.com/dw1mig7ts/image/upload/v1682899806/ViandaMarket/NewYork_-wagyustoneaxe3_wgtwty.jpg",
+    "imagen": "https://res.cloudinary.com/dw1mig7ts/image/upload/v1682899806/ViandaMarket/NewYork_-wagyustoneaxe3_wgtwty.webp",
     "gramos": "400 / 700",
     "descripcionCorte": "Corte que apenas esta ganando popularidad, su textura es similar a la arrachera pero con mucho mas sabor y suavidad.",
     "descripcionMarca": "Famosa por sus productos High Choice, lo mejor dentro de su categoria. Su proceso de añejamiento de 21 potencializa el sabor y suavidad de la carne."
@@ -211,7 +211,7 @@ export let productos = [
     "calidad": "Nacional",
     "origen": "Mexico",
     "precio": "698,00",
-    "imagen": "https://res.cloudinary.com/dw1mig7ts/image/upload/v1682899813/ViandaMarket/Ribeye-choicemx_ssi9q1.jpg",
+    "imagen": "https://res.cloudinary.com/dw1mig7ts/image/upload/v1682899813/ViandaMarket/Ribeye-choicemx_ssi9q1.webp",
     "gramos": "380 / 400 / 420 / 450",
     "descripcionCorte": "El preferido de muchos, esto gracias a su balance de sabor, suavidad y cantidad de grasa, es un corte sin hueso que proviene de la parte superior de las costillas.",
     "descripcionMarca": "El producto que manejamos en esta categoria proviene de Sonora, con un excelente sabor, calidad y textura. Su marmoleo es similar a los productos Choice y Prime."
@@ -223,7 +223,7 @@ export let productos = [
     "calidad": "Nacional",
     "origen": "Mexico",
     "precio": "456,00",
-    "imagen": "https://res.cloudinary.com/dw1mig7ts/image/upload/v1682899806/ViandaMarket/NewYork_-wagyustoneaxe3_wgtwty.jpg",
+    "imagen": "https://res.cloudinary.com/dw1mig7ts/image/upload/v1682899806/ViandaMarket/NewYork_-wagyustoneaxe3_wgtwty.webp",
     "gramos": "350 / 370 / 400",
     "descripcionCorte": "Es un corte muy bien valorado, viene del lomo de la res. Es popular por su gran suavidad y poca grasa.",
     "descripcionMarca": "El producto que manejamos en esta categoria proviene de Sonora, con un excelente sabor, calidad y textura. Su marmoleo es similar a los productos Choice y Prime."
@@ -235,7 +235,7 @@ export let productos = [
     "calidad": "Nacional",
     "origen": "Mexico",
     "precio": "367,00",
-    "imagen": "https://res.cloudinary.com/dw1mig7ts/image/upload/v1682899806/ViandaMarket/NewYork_-wagyustoneaxe3_wgtwty.jpg",
+    "imagen": "https://res.cloudinary.com/dw1mig7ts/image/upload/v1682899806/ViandaMarket/NewYork_-wagyustoneaxe3_wgtwty.webp",
     "gramos": "1.7kg / 2kg",
     "descripcionCorte": "Popularizado por su reconocimiento en espadas Brasileñas, es un corte de gran sabor y suavidad, excelente para asar en laminas pero tambien para el ahumado.",
     "descripcionMarca": "El producto que manejamos en esta categoria proviene de Sonora, con un excelente sabor, calidad y textura. Su marmoleo es similar a los productos Choice y Prime."
