cc3c6e68824e1be7fb2ae16cf0c99483e9cdcbf3
Mariano Gonzalez <oettinghaus@hotmail.com>
2023-06-21 10:36:01 -0600

    correcciones a la navbar

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,13 @@
-let cuerpoRows = document.getElementById("card");
+let cuerpoRows = document.getElementById("slider-container");
 
-const URL = "/api/cortes/";
-const URLcalidad = "/api/calidad/";
-const token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJDYXJvIzEzMTAiLCJyb2xlIjoidXNlciIsImlhdCI6MTY4NDQ2NDc4NywiZXhwIjoxNjg1Njc0Mzg3fQ.8AS2m1E_VRYGlpNKlR_qt6sLd2HYt5HTD4QlFYFkeOQ';
+const URL = "http://localhost:8080/api/cortes/";
+const URLcalidad = "http://localhost:8080/api/calidad/";
 
 const obtenerProductos = async () => {
   const resp = await fetch(URL, {
     method: 'GET',
     headers: new Headers({'Content-type': 'application/json'}),
-    mode: 'cors'
+    // mode: 'cors'
   });
 
   if (!resp.ok) {
@@ -22,8 +21,7 @@ const obtenerProductos = async () => {
 const obtenerCalidad = async () => {
   const resp = await fetch(URLcalidad, {
     method: 'GET',
-    headers: new Headers({'Content-type': 'application/json', 'authorization': `Bearer ${token}`}),
-    mode: 'cors'
+    headers: new Headers({'Content-type': 'application/json'}),
   });
 
   if (!resp.ok) {
@@ -40,30 +38,41 @@ const pintarProductos = async () => {
 
   cuerpoRows.innerHTML = ""; // Limpiar contenido existente en el div
 
-  productos.slice(0, 5).forEach(producto => {
-    const { id, nombre, idcalidades, imagen } = producto;
-    const { marca, pais, calidad } = calidad[idcalidades - 1];
+  for (let i = 0; i < 5; i++) {
+    const producto = productos[i];
+    const calidadProducto = calidad[producto.idcalidades - 1];
 
     const card = `
+    <div class="slider-track">
       <div class="slider-card">
         <div class="card" style="border: none;">
-          <img src="${imagen}" class="card-img-top">
+          <img src="${producto.imagen}" class="card-img-top">
           <div class="card-body" style="text-align: left;">
-            <h4 class="card-1">${nombre}</h4>
-            <h6 class="card-2">${marca}</h6>
-            <h6 class="card-2">${pais}</h6>
-            <h6 class="card-2">${calidad}</h6>
-            <button id="btnProducto_${id}" type="button" class="btnProducto text-center" style="background-color: #2A2E3A; color: white;">Ver producto</button>
+            <h4 class="card-1">${producto.nombre}</h4>
+            <h6 class="card-2">${calidadProducto.marca}</h6>
+            <h6 class="card-2">${calidadProducto.pais}</h6>
+            <button id="btnProducto_${producto.id}" type="button" class="btnProducto text-center" style="background-color: #2A2E3A; color: white;">Ver producto</button>
           </div>
         </div>
-      </div>`;
-
+      </div>
+      </div>
+      
+      `;
+    
     cuerpoRows.insertAdjacentHTML("beforeend", card);
-  });
+  }
 
-  // ...
+  const botones = document.getElementsByClassName("btnProducto");
+
+  for (let index = 0; index < botones.length; index++) {
+    botones[index].addEventListener("click", function (event) {
+      console.log("click" + event.target.id);
+      console.log(event.target.id.split("_")[1]);
+      localStorage.setItem("productoSeleccionado", event.target.id.split("_")[1]);
+    });
+  }
 }
 
-window.addEventListener("load", function (event) {
-  pintarProductos();
+window.addEventListener("load", async (event) => {
+  await pintarProductos();
 });
