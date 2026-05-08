2e50f5689f086d631dff1c474648499f8f9ac114
frangersal <frangersal@outlook.com>
2026-05-08 02:49:41 -0600

    Refactorice la estructura HTML y CSS de FAQ y Mayoreo, mejorando algunos detalles en footer, index y contacto

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -4,18 +4,17 @@
     .then(res => res.text())
     .then(data => {
       footer.innerHTML = data;
+      const currentYearItem = document.getElementById("current-year");
+      if (currentYearItem) {
+        currentYearItem.textContent = new Date().getFullYear();
+      }
     });
 
-
-const encabezado = document.querySelectorAll("footerCel");
+const encabezado = document.querySelectorAll(".footerCel");
 const opciones = document.getElementsByClassName("colapsadoFooter");
-
 
 for(let i=0; i < encabezado.length; i++){
     encabezado[i].addEventListener("click", () =>{
       opciones[i].classList.toggle(".colapsadoFooter-open");
-
 })
 };
-
-
